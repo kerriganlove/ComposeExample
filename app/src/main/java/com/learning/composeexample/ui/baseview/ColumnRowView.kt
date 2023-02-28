fe6de885ec10db61156383c07be3a6144f5a4a75
daesan <kerriganlove@naver.com>
2023-02-28 17:05:03 +0900

    2023-02-28
    DefaultColumn, DefaultRow 제작 중

--- app/src/main/java/com/learning/composeexample/ui/baseview/ColumnRowView.kt
+++ app/src/main/java/com/learning/composeexample/ui/baseview/ColumnRowView.kt
@@ -1,8 +1,18 @@
 package com.learning.composeexample.ui.baseview
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 
 @Composable
-fun DefaultColumn() {
+fun DefaultColumn(
+
+) {
+    Column() {
+
+    }
+}
+
+@Composable
+fun DefaultRow() {
 
 }
