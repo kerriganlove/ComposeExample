99ce6d16c8ecb7e40cd899c03e15151efa8f7553
daesan <kerriganlove@naver.com>
2023-04-14 17:01:30 +0900

    2023-04-14
    Card를 통한 Image 만들어 봄.

--- app/src/main/java/com/learning/composeexample/MainActivity.kt
+++ app/src/main/java/com/learning/composeexample/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.learning.composeexample
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.learning.composeexample.ui.baseview.DefaultCard
 import com.learning.composeexample.ui.baseview.DefaultColumn
 import com.learning.composeexample.ui.baseview.DefaultRow
 
@@ -13,8 +18,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DefaultColumn()
-            DefaultRow()
+            val painter = painterResource(id = R.drawable.test_image)
+            val title = "Test Image"
+            val description = "Lovelyz Album Shot"
+            Column() {
+                DefaultCard(modifier = Modifier.fillMaxWidth(0.4f).padding(12.dp),painter = painter, contentDescription = description, title = title)
+                Spacer(modifier = Modifier.fillMaxHeight(0.1f))
+                DefaultCard(modifier = Modifier.fillMaxWidth(0.3f).padding(12.dp), painter = painter, contentDescription = "SnackBar Test" , title = "Lovelyz so STRANGE")
+            }
         }
     }
 }
--- app/src/main/java/com/learning/composeexample/ui/baseview/ColumnRowView.kt
+++ app/src/main/java/com/learning/composeexample/ui/baseview/ColumnRowView.kt
@@ -1,7 +1,23 @@
 package com.learning.composeexample.ui.baseview
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun DefaultColumn(
@@ -15,4 +31,38 @@ fun DefaultColumn(
 @Composable
 fun DefaultRow() {
 
+}
+
+@Composable
+@Preview
+fun DefaultCard(
+    modifier : Modifier = Modifier,
+    painter : Painter,
+    contentDescription : String,
+    title : String
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(20.dp),
+
+    ) {
+        Box(Modifier.clickable {
+
+        }) {
+            Image(painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.fillMaxWidth())
+            Text(text = title,
+                style = TextStyle(color = Color.White, fontSize = 14.sp),
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(5.dp))
+        }
+    }
+}
+
+@Composable
+fun DefaultSnackBar(description : String) {
+    val name by remember { mutableStateOf(description) }
 }
