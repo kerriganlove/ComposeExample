875c8ab13253a063c956c1deda9ff72b2b4f03a5
daesan <kerriganlove@naver.com>
2023-04-17 14:59:00 +0900

    2023-04-14
    Snackbar 기본 예제 만들어봤는데 뭔가 이상하긴 해서 이리저리 고쳐봐야하는 상황에 놓여져버렸는데 과연 이걸 어떻게 처리할 것인가 굉장히 무섭네요

--- app/src/main/java/com/learning/composeexample/ui/baseview/ColumnRowView.kt
+++ app/src/main/java/com/learning/composeexample/ui/baseview/ColumnRowView.kt
@@ -5,19 +5,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 @Composable
 fun DefaultColumn(
@@ -33,36 +30,41 @@ fun DefaultRow() {
 
 }
 
+
+// State Hoisting?
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-@Preview
 fun DefaultCard(
     modifier : Modifier = Modifier,
     painter : Painter,
     contentDescription : String,
-    title : String
+    title : String,
 ) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(20.dp),
+    val defaultScope = rememberCoroutineScope()
+    val snackBarHost = remember { SnackbarHostState() }
+    Scaffold(
+        snackbarHost = {SnackbarHost(snackBarHost)},
+    ) { value ->
+        Card(
+            modifier = modifier.fillMaxWidth().padding(value),
+            shape = RoundedCornerShape(20.dp),
 
-    ) {
-        Box(Modifier.clickable {
-
-        }) {
-            Image(painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.FillBounds,
-                modifier = Modifier.fillMaxWidth())
-            Text(text = title,
-                style = TextStyle(color = Color.White, fontSize = 14.sp),
-            modifier = Modifier
-                .align(Alignment.BottomStart)
-                .padding(5.dp))
+            ) {
+            Box(Modifier.clickable {
+                defaultScope.launch {
+                    snackBarHost.showSnackbar("clicked $title")
+                }
+            }) {
+                Image(painter = painter,
+                    contentDescription = contentDescription,
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier.fillMaxWidth())
+                Text(text = title,
+                    style = TextStyle(color = Color.White, fontSize = 14.sp),
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .padding(5.dp))
+            }
         }
     }
-}
-
-@Composable
-fun DefaultSnackBar(description : String) {
-    val name by remember { mutableStateOf(description) }
 }
