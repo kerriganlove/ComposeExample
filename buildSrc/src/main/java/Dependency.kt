5f243ae890374b7d2cd74049096d06da759daea9
daesan <kerriganlove@naver.com>
2023-05-16 17:36:35 +0900

    2023-04-24
    1. Groovy -> Kotlin Script 변환 중

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -1,3 +1,5 @@
+import org.gradle.api.Project
+
 object Version {
     const val GLIDE_COMPOSE_VERSION = "1.0.0-alpha.1"
     const val CORE_KTX_VERSION = "1.10.1"
@@ -7,7 +9,7 @@ object Version {
     const val MATERIAL_3_VERSION = "1.1.0"
 }
 
-object AndroidX {
+object AppDependency {
     const val CORE_KTX = "androidx.core:core-ktx:1.9.0:${Version.CORE_KTX_VERSION}"
     const val LIFECYCLE_KTX = "androidx.lifecycle:lifecycle-runtime-ktx:${Version.LIFECYCLE_KTX_VERSION}"
     const val ACTIVITY_COMPOSE = "androidx.activity:activity-compose:${Version.ACTIVITY_COMPOSE_VERSION}"
@@ -17,16 +19,35 @@ object AndroidX {
 
     const val COMPOSE_MATERIAL_3 = "androidx.compose.material3:material3:${Version.MATERIAL_3_VERSION}"
 
-}
-
-object Kotlin {
+    const val GLIDE = "com.github.bumptech.glide:compose:${Version.GLIDE_COMPOSE_VERSION}"
 
 }
 
 object AppTest {
-
+    const val JUNIT = "junit:junit:4.13.2"
 }
 
-object Github {
-    const val Glide = "com.github.bumptech.glide:compose:${Version.GLIDE_COMPOSE_VERSION}"
+object RejectDependency {
+    const val HILT_ANDROID = ""
+    const val KOIN = ""
 }
+
+object AppAndroid {
+    val libList = arrayOf(
+        AppDependency.CORE_KTX,
+        AppDependency.LIFECYCLE_KTX,
+        AppDependency.ACTIVITY_COMPOSE,
+        AppDependency.COMPOSE_UI,
+        AppDependency.COMPOSE_UI_PREVIEW,
+        AppDependency.COMPOSE_MATERIAL_3,
+        AppDependency.GLIDE
+    )
+
+    val rejectionLibList = arrayOf(
+        RejectDependency.KOIN
+    )
+    val testLibList = arrayOf(
+        AppTest.JUNIT
+    )
+}
+
